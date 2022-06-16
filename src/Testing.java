df595d01c833fc4c8ed7b24e3d1c33caf2c216d2
Connor <connorhaylers@gmail.com>
2022-06-16 18:16:11 +0100

    inital commit

---
+++ src/Testing.java
@@ -0,0 +1,106 @@
+import java.awt.event.HierarchyBoundsAdapter;
+import java.util.Scanner;
+
+
+public class Testing {
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextLine()); /
+//        System.out.println(input.nextLine());
+//        System.out.println(input.nextLine());
+//        System.out.println(input.nextInt());//
+//        System.out.println(input.nextLine());//
+//        System.out.println(input.nextLine());
+
+
+//Hi
+//My name is Connor
+//My age is
+//22
+//Nice to meet you
+
+
+        //3483
+        //Cans of soup
+
+        //3483\n
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+        input.nextLine();
+        //System.out.println(input.nextLine());
+        System.out.println(input.nextLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        boolean cake = true;
+//        boolean chocolate = false;
+//        boolean vanilla = false;
+//        boolean iceCream = true;
+//        boolean banana = false;
+//        boolean strawberry = true;
+//        boolean mint = false;
+//
+//        if(cake){
+//            if(chocolate){
+//                System.out.println("get chocolate cake");
+//            }else if(vanilla){
+//                System.out.println("get vanilla cake");
+//            }else{
+//                System.out.println("get whatever cake they have");
+//            }
+//        }else if(iceCream){
+//            if(banana){
+//                System.out.println("get banana icecream");
+//            }else if(strawberry){
+//                System.out.println("get strawberry icecream");
+//            }else if(mint){
+//                System.out.println("get mint icecream");
+//            }
+//        }
+
+
+
+
+
+
+
+
+//       double cost = 10000;
+//       int years = 0;
+//       double totalCost = 0;
+//       do{
+//           cost = cost * 1.06;
+//           years++;
+//           if(years > 10){
+//               //the cost of uni for 4 years
+//               //get the cost of uni each year with inflation
+//               //total the cost for four years
+//               System.out.println(cost);
+//               totalCost = totalCost + cost;
+//           }
+//           System.out.println(years);
+//           System.out.println(cost);
+//
+//       }while(years < 14);
+//       System.out.println("The total cost of university for 4 years in 10 years time is " + totalCost);
+
+//         int x = 0;
+//         do{
+//             x = x + 1;
+//         }while(x < 10);
+//         System.out.println(x);
+    }
+}
